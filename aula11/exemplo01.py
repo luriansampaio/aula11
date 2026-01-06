9f6bd42b8a836b3a9a3433d47e86911cd9589073
luriansampaio <luriansampaio@gmail.com>
2026-01-06 19:35:26 -0300

    adicona

--- aula11/exemplo01.py
+++ aula11/exemplo01.py
@@ -8,7 +8,12 @@ import random
 
 #------------------------------------------------
 # Gerar números decimais 
-n_decimal = random.uniform(1,10)
-numero_decimal = round(n_decimal,3) # arredonda
-print(f'{n_decimal:.2f}') #formata para duas casas 
-print(numero_decimal)
+# n_decimal = random.uniform(1,10)
+# numero_decimal = round(n_decimal,3) # arredonda
+# print(f'{n_decimal:.2f}') #formata para duas casas 
+# print(numero_decimal)
+
+#---------------------------------------------------
+# Gerar números aleatórios dentro de uma lista
+lst_numeros = [random.randint(1,10) for num in range(5)]
+print(lst_numeros)
