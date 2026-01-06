11bcdc31a937bef138d8ae6f5de21c94e7b8b764
luriansampaio <luriansampaio@gmail.com>
2026-01-06 19:26:02 -0300

    atualiza aula 11

--- aula11/exemplo01.py
+++ aula11/exemplo01.py
@@ -1,7 +1,14 @@
-# Bibliotecas
+# # Bibliotecas
 import random
 
-#random.randint = números inteiros
-n = random.randint(1,10)
-m = random.randint(1,10)
-print (n,m)
+# #random.randint = números inteiros
+# n = random.randint(1,10)
+# m = random.randint(1,10)
+# print (n,m)
+
+#------------------------------------------------
+# Gerar números decimais 
+n_decimal = random.uniform(1,10)
+numero_decimal = round(n_decimal,3) # arredonda
+print(f'{n_decimal:.2f}') #formata para duas casas 
+print(numero_decimal)
